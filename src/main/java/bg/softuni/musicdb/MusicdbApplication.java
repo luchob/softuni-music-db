a04569b69ee4f1b86b901f72cd4e5142082198ac
rd-lachezar-balev <lachezar.balev@rewe-digital.com>
2021-02-26 20:22:53 +0200

    Initial commit

---
+++ src/main/java/bg/softuni/musicdb/MusicdbApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.musicdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicdbApplication.class, args);
+	}
+
+}
